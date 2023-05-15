fa86a367d03b50b7e43c3214a99b8f34956bd6a5
Jakob Madsen <36546134+jsture@users.noreply.github.com>
2023-05-15 12:34:49 +0200

    Update computeMSMS.py

--- masif/source/triangulation/computeMSMS.py
+++ masif/source/triangulation/computeMSMS.py
@@ -23,7 +23,7 @@ def computeMSMS(pdb_file, protonate=True):
         print("Error - pdb2xyzrn is deprecated.")
         sys.exit(1)
     # Now run MSMS on xyzrn file
-    probe_start = 2.6
+    probe_start = 1.505
     vert_exists = False
     fudge = 0
     while not vert_exists:
@@ -49,7 +49,7 @@ def computeMSMS(pdb_file, protonate=True):
             vert_exists = os.path.exists(file_base + ".vert")
         except Exception as e:
             print(e)
-            fudge += 0.01
+            fudge += 0.05
             if probe_radius > 5.0 or probe_radius < 1.3:
                 print("Error: MSMS failed to run.")
                 sys.exit(1)
